6a589ffcd91cd5f713ec5ba149e680b87c38d896
hi33741 <hi33741@naver.com>
2021-12-30 11:49:14 +0900

    스타일컴포넌트와 mui

--- pages/journal.tsx
+++ pages/journal.tsx
@@ -1,11 +1,72 @@
-import React from "react";
-import AppLayout from "../component/AppLayout";
+import React from 'react';
+import AppLayout from '../component/AppLayout';
+import Box, { BoxProps } from '@mui/material/Box';
+
+import styled from 'styled-components';
+
+const size = {
+  mobileS: '320px',
+  mobileM: '375px',
+  mobileL: '425px',
+  tablet: '768px',
+  laptop: '1024px',
+  laptopL: '1440px',
+  desktop: '2560px',
+};
+
+export const device = {
+  mobileS: `(max-width: ${size.mobileS})`,
+  mobileM: `(max-width: ${size.mobileM})`,
+  mobileL: `(max-width: ${size.mobileL})`,
+  tablet: `(max-width: ${size.tablet})`,
+  laptop: `(max-width: ${size.laptop})`,
+  laptopL: `(max-width: ${size.laptopL})`,
+  desktop: `(max-width: ${size.desktop})`,
+  desktopL: `(max-width: ${size.desktop})`,
+};
+
+const Div = styled.div`
+  display: flex;
+  width: '100%';
+
+  @media ${device.tablet} {
+    flex-direction: column;
+  }
+`;
+
+const Item = (props: BoxProps) => {
+  const { sx, ...other } = props;
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        bgcolor: 'primary.main',
+        color: 'black',
+        width: '95%',
+        height: 500,
+        m: 1,
+        borderRadius: 1,
+        textAlign: 'center',
+        fontSize: '1rem',
+        fontWeight: '700',
+
+        ...sx,
+      }}
+      {...other}
+    />
+  );
+};
 
 const journal = () => {
   return (
     <>
       <AppLayout>
-        <div>journal</div>
+        <Div>
+          <Item>1</Item>
+          <Item>2</Item>
+        </Div>
       </AppLayout>
     </>
   );
